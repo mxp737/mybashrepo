23088f56ae4b3843d20f475bd043b8fc007432ce
root <root@ip-172-31-91-217.ec2.internal>
2023-09-30 03:14:55 +0000

    Updated Reminder in cal.sh

--- cal.sh
+++ cal.sh
@@ -5,10 +5,10 @@ echo "1. Add"
 echo "2. Sub"
 echo "3. Mul"
 echo "4. Div"
-
+echo "5. Reminder"
 read selection
 
-if [ $selection == "1" ] || [ $selection == "2" ] || [ $selection == "3" ] || [ $selection == "4" ]; then
+if [ $selection == "1" ] || [ $selection == "2" ] || [ $selection == "3" ] || [ $selection == "4" ] || [ $selection == "5" ]; then
     echo "enter firstNumber"
     read firstNumber
 
@@ -37,7 +37,10 @@ div(){
     divResult=$(( $1 / $2 ))
     echo "Div result is: $divResult"
 }
-
+reminder(){
+   reminderResult=$(( $1 % $2 ))
+   echo "Reminder result is: $reminderResult"
+}
 if [ $selection == "1" ]; then
     add $firstNumber $secondNumber
 elif [ $selection == "2" ]; then
@@ -46,4 +49,7 @@ elif [ $selection == "3" ]; then
     mul $firstNumber $secondNumber
 elif [ $selection == "4" ]; then
     div $firstNumber $secondNumber
+elif [ $selection == "5" ]; then
+    reminder $firstNumber $secondNumber
+
 fi
